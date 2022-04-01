f89b3e6778ee472374eae17c083ebf1181ff7ea2
Dinar Agzyamov <mail@deenar.ru>
2022-04-01 22:30:22 +0300

    Revert "quicksave"
    This reverts commit 772ee2d592a2f6e8240aecbe8ddd6ab80146baca.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,17 @@
-import React from "react"
-import Feedback from "./feedback/Feedback"
-import Statistics from "./statistics/Statistics"
-
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
-  onLeaveFeedback = (option) => {
-    this.setState((prevState) => ({
-      [option]: prevState[option] + 1,
-    }));
-  };
-  countTotalFeedback = () => {
-     return Object.values(this.state).reduce((a, b) => (a += b));
-  };
-  countPositiveFeedbackPercentage = () => { };
-  render() {
-    const { good, neutral, bad } = this.state
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <>
-        <Feedback
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.onLeaveFeedback} />
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positivePercentage={positivePercentage} />
-      </>
-    )
-  }
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
 };
-export default App
